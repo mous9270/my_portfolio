ee6e418d6521979d7be582e12b23c24bc8087021
mous9270 <mous9270222@gmail.com>
2024-10-17 15:04:37 +0530

    The model in home page is almost ready.

--- src/components/RenderModel.jsx
+++ src/components/RenderModel.jsx
@@ -7,13 +7,13 @@ import React, { Suspense } from "react";
 const RenderModel = ({ children, className }) => {
   return (
     <Canvas
-      className={clsx("w-screen h-screen -z-10 relative", className)}
+      className={clsx("w-screen h-screen -z-10  relative", className)}
       shadows={false}
       dpr={[1, 2]}
       // dpr is the device pixel ratio. Here we are setting it to 1 and 2 for retina displays to prevent blurriness in the model rendering on high resolution screens.
     >
       <Suspense fallback={null}>{children}</Suspense>
-      <Environment preset="dawn" />
+      <Environment preset="night" />
     </Canvas>
   );
 };
