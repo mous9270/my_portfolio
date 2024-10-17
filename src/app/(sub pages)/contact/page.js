8fef1fd062e745cff13c68e177827978ca1ccdcf
mous9270 <mous9270222@gmail.com>
2024-10-17 19:15:29 +0530

    Except colors all updates are done, I am going to deploy it now.

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -20,15 +20,10 @@ export default function Contact() {
       <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
         <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
           <h1 className="text-accent font-semibold text-center text-4xl capitalize">
-            summon the wizard
+            Invoke the Programmer
           </h1>
           <p className="text-center font-light text-sm xs:text-base">
-            Step into the circle of enchantment and weave your words into the
-            fabric of the cosmos. Whether you seek to conjure collaborations,
-            unlock mysteries, or simply share tales of adventure, your messages
-            are treasured scrolls within this realm. Use the form below to send
-            your missives through the ethereal network, and await the whisper of
-            magic in response.
+             Enter the digital realm and infuse your words into the code of innovation. Whether you seek collaboration, wish to unravel complex challenges, or share tales from your journey, your messages are the catalyst for new possibilities in this universe. Use the form below to transmit your thoughts through the virtual network, and await the response that will bring your visions to reality.
           </p>
         </div>
         <Form />
